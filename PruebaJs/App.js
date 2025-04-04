5f4eeba026b828f771389a97340de8913efb2584
Vicente <vicente.practicas@pipeline.es>
2025-04-04 13:58:40 +0200

    asd

--- PruebaJs/App.js
+++ PruebaJs/App.js
@@ -1,8 +1,8 @@
 // App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigation from '../src/navigation/AppNavigation';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import AppNavigation from './src/navigation/AppNavigation';
+
 
 
 export default function App() {
